5aff83b9a88377286e8398f09b92d3485c29e90f
emonzon42 <elijah.monzon42@gmail.com>
2021-02-18 13:11:54 -0500

    created javadoc

---
+++ Assignments/Assignment1/hdb_data/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"hdb.data.nonrelational"},{"l":"hdb.data.relational"}]
--- Assignments/Assignment1/hdb_data/src/module-info.java
+++
@@ -1,5 +0,0 @@
-module hdb_data {
-	exports hdb.data.relational;
-	exports hdb.data.nonrelational;
-	exports hdb.data;
-}
